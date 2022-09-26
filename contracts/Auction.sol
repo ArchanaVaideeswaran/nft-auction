6e9cc3a4f52263e1a01a7df2a7233cbc95f10b99
Archana Vaideeswaran <archanav1929@gmail.com>
2022-09-26 12:04:54 +0530

    function settle auction and cancel auction

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -9,14 +9,10 @@ import "@openzeppelin/contracts/interfaces/IERC165.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Auction is ERC721Holder, ReentrancyGuard {
-    
-    // Type Declarations
     struct Listing {
         address payable seller;
         uint startingPrice;
-        uint highestBid;
-        address payable highestBidder;
-        address paymentToken;
+        Bid highestBid;
         uint32 startTime;
         uint32 duration;
         uint32 timeBuffer;
@@ -29,12 +25,9 @@ contract Auction is ERC721Holder, ReentrancyGuard {
         address paymentToken;
     }
 
-    // State Variables
-    uint _listingId;
-    mapping(address => mapping(uint => Listing)) _listings;
-    mapping(address => mapping(uint => mapping(address => Bid))) _bids;
+    mapping(address => mapping(uint => Listing)) private _listings;
+    mapping(address => mapping(uint => mapping(address => Bid))) private _bids;
 
-    // Events
     event AuctionCreated(
         address indexed nft,
         uint tokenId,
@@ -53,14 +46,18 @@ contract Auction is ERC721Holder, ReentrancyGuard {
         address indexed bidder,
         uint amount
     );
-
-    // Modifiers
-
-    // Constructor
-
-    receive() external payable {}
-
-    // External Functions
+    event AuctionSettled(
+        address indexed nft,
+        uint tokenId,
+        address indexed seller,
+        address indexed bidder,
+        uint amount
+    );
+    event AuctionCancelled(
+        address indexed nft,
+        uint tokenId,
+        address indexed seller
+    );
 
     function createAuction(
         address nft,
@@ -104,7 +101,7 @@ contract Auction is ERC721Holder, ReentrancyGuard {
 
         item.seller = payable(msg.sender);
         item.startingPrice = startingPrice;
-        item.paymentToken = paymentToken;
+        item.highestBid.paymentToken = paymentToken;
         item.startTime = startTime;
         item.duration = duration;
         item.timeBuffer = timeBuffer;
@@ -125,7 +122,7 @@ contract Auction is ERC721Holder, ReentrancyGuard {
 
         require(item.seller != address(0), "Auction item does not exists");
         require(
-            amount >= (item.highestBid + item.ticSize),
+            amount >= (item.highestBid.amount + item.ticSize),
             "Minimum tic size not met"
         );
         require(
@@ -137,18 +134,21 @@ contract Auction is ERC721Holder, ReentrancyGuard {
             "Auction ended"
         );
         require(
-            item.paymentToken != address(0),
+            item.highestBid.paymentToken != address(0),
             "Payment token is not ERC20"
         );
 
         Bid storage _bid = _bids[nft][tokenId][msg.sender];
 
-        _bid.bidder = payable(msg.sender);
-        _bid.amount += amount;
-        _bid.paymentToken = item.paymentToken;
+        if(_bid.bidder == address(0)) {
+            _bid.bidder = payable(msg.sender);
+            _bid.amount += amount;
+            _bid.paymentToken = item.highestBid.paymentToken;
+        } else {
+            _bid.amount += amount;
+        }
 
-        item.highestBid = amount;
-        item.highestBidder = payable(msg.sender);
+        item.highestBid = _bid;
 
         bool extended;
         uint32 timeRemaining = blockTimeStamp - item.startTime;
@@ -158,7 +158,7 @@ contract Auction is ERC721Holder, ReentrancyGuard {
             extended = true;
         }
 
-        IERC20(item.paymentToken).transferFrom(
+        IERC20(_bid.paymentToken).transferFrom(
             msg.sender,
             address(this),
             amount
@@ -174,7 +174,7 @@ contract Auction is ERC721Holder, ReentrancyGuard {
 
         require(item.seller != address(0), "Auction item does not exists");
         require(
-            amount >= (item.highestBid + item.ticSize),
+            amount >= (item.highestBid.amount + item.ticSize),
             "Minimum tic size not met"
         );
         require(
@@ -186,18 +186,21 @@ contract Auction is ERC721Holder, ReentrancyGuard {
             "Auction ended"
         );
         require(
-            item.paymentToken == address(0),
+            item.highestBid.paymentToken == address(0),
             "Payment token is not ETH"
         );
 
         Bid storage _bid = _bids[nft][tokenId][msg.sender];
 
-        _bid.bidder = payable(msg.sender);
-        _bid.amount += amount;
-        _bid.paymentToken = item.paymentToken;
+        if(_bid.bidder == address(0)) {
+            _bid.bidder = payable(msg.sender);
+            _bid.amount += amount;
+            _bid.paymentToken = item.highestBid.paymentToken;
+        } else {
+            _bid.amount += amount;
+        }
 
-        item.highestBid = amount;
-        item.highestBidder = payable(msg.sender);
+        item.highestBid = _bid;
 
         bool extended;
         uint32 timeRemaining = blockTimeStamp - item.startTime;
@@ -210,17 +213,66 @@ contract Auction is ERC721Holder, ReentrancyGuard {
         emit BidPlaced(nft, tokenId, _bid.bidder, amount, extended);
     }
 
-    function settleAuction(address nft, uint tokenId) external {}
+    function settleAuction(address nft, uint tokenId) external {
+        Listing memory item = getListing(nft, tokenId);
+        Bid memory _bid = getBid(nft, tokenId, msg.sender);
+        uint32 blockTimeStamp = uint32(block.timestamp);
 
-    function cancelAuction(address nft, uint tokenId) external {} 
+        require(item.seller != address(0), "Auction item does not exists");
+        require(
+            blockTimeStamp > (item.startTime + item.duration),
+            "Auction not ended"
+        );
+        require(item.highestBid.bidder == msg.sender, "Caller not highest bidder");
+
+        delete _listings[nft][tokenId];
+        delete _bids[nft][tokenId][msg.sender];
+
+        IERC721(nft).safeTransferFrom(
+            address(this),
+            item.highestBid.bidder,
+            tokenId
+        );
+
+        if(_bid.paymentToken != address(0)) {
+            IERC20(_bid.paymentToken).transferFrom(
+                _bid.bidder,
+                item.seller,
+                _bid.amount
+            );
+        } else {
+            (bool succes, ) = payable(item.seller).call{value: _bid.amount}("");
+            require(succes, "ETH transfer failed");
+        }
+
+        emit AuctionSettled(nft, tokenId, item.seller, _bid.bidder, _bid.amount);
+    }
+
+    function cancelAuction(address nft, uint tokenId) external {
+        Listing memory item = getListing(nft, tokenId);
+
+        require(item.seller != address(0), "Auction item does not exists");
+        require(item.seller == msg.sender, "Caller is not seller");
+        require(item.highestBid.bidder == address(0), "Bid(s) exist");
+
+        delete _listings[nft][tokenId];
+
+        IERC721(nft).safeTransferFrom(
+            address(this),
+            item.seller,
+            tokenId
+        );
+
+        emit AuctionCancelled(nft, tokenId, item.seller);
+    } 
 
     function claimBid(address nft, uint tokenId) external nonReentrant {
-        Listing memory item = _listings[nft][tokenId];
-        Bid memory _bid = _bids[nft][tokenId][msg.sender];
+        Listing memory item = getListing(nft, tokenId);
+        Bid memory _bid = getBid(nft, tokenId, msg.sender);
 
         require(_bid.amount > 0, "No active bids");
         require(
-            item.highestBidder != msg.sender,
+            item.highestBid.bidder != msg.sender,
             "Highest bidder cannot claim bid"
         );
 
@@ -240,9 +292,18 @@ contract Auction is ERC721Holder, ReentrancyGuard {
         emit BidClaimed(nft, tokenId, _bid.bidder, _bid.amount);
     }
 
-    // Public Functions
+    function getListing(
+        address nft,
+        uint tokenId
+    ) public view returns (Listing memory) {
+        return _listings[nft][tokenId];        
+    }
 
-    // Internal Functions
-
-    // Private Fuctions
+    function getBid(
+        address nft,
+        uint tokenId,
+        address bidder
+    ) public view returns (Bid memory) {
+        return _bids[nft][tokenId][bidder];
+    }
 }
