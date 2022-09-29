241f69e2bb51a58873b3c2262be6cd460667f9fd
Archana Vaideeswaran <archanav1929@gmail.com>
2022-09-29 18:32:39 +0530

    can execute bid

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -12,7 +12,7 @@ contract DutchAuction is ERC721Holder, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     struct Listing {
-        address payable seller;
+        address seller;
         uint startPrice;
         uint endPrice;
         uint32 startTime;
@@ -20,6 +20,8 @@ contract DutchAuction is ERC721Holder, ReentrancyGuard {
         address paymentToken;
     }
 
+    uint public _minimumAuctionLengthInSeconds;
+    address public owner;
     mapping(address => mapping(uint => Listing)) private _listings;
 
     event AuctionCreated(
@@ -27,13 +29,29 @@ contract DutchAuction is ERC721Holder, ReentrancyGuard {
         uint tokenId,
         address indexed seller
     );
-    event AuctionSold(
+    event AuctionItemSold(
         address indexed nft,
         uint tokenId,
         address indexed seller,
         address indexed buyer,
         uint amount
     );
+    event AuctionCancelled(
+        address indexed nft,
+        uint tokenId,
+        address indexed seller
+    );
+    event UpdatedMinAuctionLength(uint lengthInSeconds);
+
+    constructor(uint minimumAuctionLengthInSeconds) {
+        owner = msg.sender;
+        setMinimumAuctionLength(minimumAuctionLengthInSeconds);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller not owner");
+        _;
+    }
 
     function createAuction(
         address nft,
@@ -48,10 +66,10 @@ contract DutchAuction is ERC721Holder, ReentrancyGuard {
             IERC165(nft).supportsInterface(type(IERC721).interfaceId),
             "Token contract does not support interface IERC721"
         );
-        address owner = IERC721(nft).ownerOf(tokenId);
+        address tokenOwner = IERC721(nft).ownerOf(tokenId);
         require(
-            msg.sender == owner || 
-            IERC721(nft).isApprovedForAll(owner, msg.sender),
+            msg.sender == tokenOwner || 
+            IERC721(nft).isApprovedForAll(tokenOwner, msg.sender),
             "Caller is not owner or operator"
         );
         require(startPrice > 0, "Starting price too small");
@@ -62,14 +80,17 @@ contract DutchAuction is ERC721Holder, ReentrancyGuard {
             "Payment token is neither zero (ETH) nor supports interface IERC20"
         );
         require(
-            startTime >= uint32(block.timestamp) || startTime == 0,
+            startTime >= uint32(block.timestamp),
             "Start time must be >= block timestamp"
         );
-        require(duration >= 15 minutes, "Auction duration must be >= 15 mins");
+        require(
+            duration >= _minimumAuctionLengthInSeconds,
+            "Duration must be greater than minimum auction duration"
+        );
 
         Listing storage item = _listings[nft][tokenId];
 
-        item.seller = payable(msg.sender);
+        item.seller = msg.sender;
         item.startPrice = startPrice;
         item.endPrice = endPrice;
         item.startTime = startTime;
@@ -90,41 +111,44 @@ contract DutchAuction is ERC721Holder, ReentrancyGuard {
 
         require(item.seller != address(0), "Auction item does not exist");
         require(msg.sender != item.seller, "Caller cannot be seller");
-        require(uint32(block.timestamp) >= item.startTime, "Auction not started");
-        require(
-            uint32(block.timestamp) < (item.startTime + item.duration),
-            "Auction ended"
-        );
-
-        uint elapsedTime = uint(uint32(block.timestamp) - item.startTime);
-        uint currentPrice = getCurrentPrice(
-            elapsedTime,
-            item.duration,
-            item.startPrice,
-            item.endPrice
-        );
 
         if(item.paymentToken == address(0)) {
             amount = msg.value;
         }
-        require(amount >= currentPrice, "Price too small");
+
+        bool isExecutable = canExecuteBid(item, amount);        
+        require(isExecutable, "Cannot execute bid");
 
         delete _listings[nft][tokenId];
 
+        _handlePayment(item.paymentToken, msg.sender, item.seller, amount);
+        
         _handleNftTransfer(nft, tokenId, address(this), msg.sender);
 
-        _handlePayment(item.paymentToken, msg.sender, item.seller, amount);
-
-        emit AuctionSold(nft, tokenId, item.seller, msg.sender, amount);
+        emit AuctionItemSold(nft, tokenId, item.seller, msg.sender, amount);
     }
 
-    function getCurrentPrice(
-        uint elapsedTime,
-        uint duration,
-        uint startPrice,
-        uint endPrice
-    ) public pure returns (uint currentPrice) {
-        currentPrice = ((startPrice - endPrice) * (elapsedTime)) / duration;
+    function cancelAuction(address nft, uint tokenId) external nonReentrant {
+        Listing memory item = getListing(nft, tokenId);
+
+        require(item.seller != address(0), "Auction item does not exists");
+        require(item.seller == msg.sender, "Caller is not seller");
+
+        delete _listings[nft][tokenId];
+
+        _handleNftTransfer(nft, tokenId, address(this), item.seller);
+
+        emit AuctionCancelled(nft, tokenId, item.seller);
+    }
+
+    function setMinimumAuctionLength(uint minimumAuctionLengthInSeconds) public onlyOwner {
+        require(
+            minimumAuctionLengthInSeconds >= 15 minutes,
+            "Auction length must be > 15 minutes"
+        );
+        _minimumAuctionLengthInSeconds = minimumAuctionLengthInSeconds;
+        
+        emit UpdatedMinAuctionLength(minimumAuctionLengthInSeconds);
     }
 
     function getListing(
@@ -142,7 +166,7 @@ contract DutchAuction is ERC721Holder, ReentrancyGuard {
     ) internal {
         if(token != address(0)) {
             IERC20(token).safeTransferFrom(from, to, amount);
-        } else if(to != address(this)) {
+        } else {
             (bool success, ) = payable(to).call{value: amount}("");
             require(success, "ETH transfer failed");
         }
@@ -155,5 +179,25 @@ contract DutchAuction is ERC721Holder, ReentrancyGuard {
         address to
     ) internal {
         IERC721(token).safeTransferFrom(from, to, tokenId);
+    }
+
+    function canExecuteBid(
+        Listing memory item,
+        uint amount
+    ) private view returns (bool) {
+        uint startTime = item.startTime;
+        uint endTime = uint(item.startTime + item.duration);
+        uint blockTimestamp = block.timestamp;
+
+        require(startTime <= blockTimestamp, "Auction not started");
+        require(endTime >= blockTimestamp, "Auction ended");
+
+        uint currentAuctionPrice = item.startPrice - (
+            ((item.startPrice - item.endPrice) * (blockTimestamp - startTime)) 
+            / 
+            (endTime - startTime)
+        );
+
+        return (amount >= currentAuctionPrice);
     }
 }
