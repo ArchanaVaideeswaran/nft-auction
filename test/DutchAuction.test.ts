d0ef5aee7e5f4cc898f4eec7224e0f8014298985
Archana Vaideeswaran <archanav1929@gmail.com>
2022-10-06 12:39:33 +0530

    testcases completed for Dutch auction

--- test/DutchAuction.test.ts
+++ test/DutchAuction.test.ts
@@ -1,6 +1,6 @@
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
+import { expect, use } from "chai";
 import { ethers } from "hardhat";
 import { it } from "mocha";
 import { Avengers, Dummy, DutchAuction, WETH } from "../typechain-types";
@@ -22,7 +22,8 @@ describe("Dutch Auction", () => {
     let auction: DutchAuction;
     let nft: Avengers;
     let dummy: Dummy;
-    let MIN_AUCTION_LENGTH_IN_SECONDS = 15 * 60;
+    const MIN_AUCTION_LENGTH_IN_SECONDS = 15 * 60;
+    const FIVE_MINS_IN_SECONDS = 5 * 60;
 
     before(async () => {
         [owner, user1, user2, ...users] = await ethers.getSigners();
@@ -48,7 +49,7 @@ describe("Dutch Auction", () => {
         await dummy.deployed();
     });
 
-    describe("Function create auction", () => {
+    xdescribe("Function create auction", () => {
         // function parameters
         let token;
         let tokenId;
@@ -171,9 +172,8 @@ describe("Dutch Auction", () => {
             tokenId = 1;
             startPrice = toWei("10");
             endPrice = toWei("2");
-            startTime = await time.latest();
-            startTime += 5 * 60; // auction starts in 5 minutes from the previous block.
-            duration = MIN_AUCTION_LENGTH_IN_SECONDS;
+            startTime = (await time.latest()) + FIVE_MINS_IN_SECONDS;
+            duration = (MIN_AUCTION_LENGTH_IN_SECONDS * 2);
             paymentToken = weth.address;
 
             await nft.approve(auction.address, 1);
@@ -190,7 +190,7 @@ describe("Dutch Auction", () => {
         })
     });
 
-    describe("Function buy auction item", () => {
+    xdescribe("Function buy auction item", () => {
         // function parameters
         let token;
         let tokenId;
@@ -264,9 +264,8 @@ describe("Dutch Auction", () => {
             amount = toWei("8");
 
             startTime = (await auction.getListing(token, tokenId)).startTime;
-            const FIVE_MINS_IN_SECONDS = 5 * 60;
             let increase = (
-                startTime
+                (await time.latest())
                 + FIVE_MINS_IN_SECONDS
             );
             await time.increaseTo(increase);
@@ -280,5 +279,79 @@ describe("Dutch Auction", () => {
                 amount
             )).to.changeTokenBalances(nft, [auction, user1], [-1, 1]);
         });
+    });
+
+    describe("Function cancel auction", () => {
+        // function parameters
+        let token;
+        let tokenId;
+
+        it("should validate input parameters",async () => {
+            token = nft.address;
+            tokenId = 2;
+
+            await expect(auction.connect(user1).cancelAuction(
+                token,
+                tokenId // token ID 2 was not put for auction yet
+            )).to.be.rejectedWith("Auction item does not exists");
+
+            await nft.connect(user1).approve(auction.address, tokenId);
+
+            let startPrice = toWei("18");
+            let endPrice = toWei("12");
+            let startTime = (await time.latest()) + FIVE_MINS_IN_SECONDS;
+            let duration = MIN_AUCTION_LENGTH_IN_SECONDS + FIVE_MINS_IN_SECONDS; // 20 minutes
+            let paymentToken = weth.address;
+
+            await expect(auction.connect(user1).createAuction(
+                token,
+                tokenId,
+                startPrice,
+                endPrice,
+                startTime,
+                duration,
+                paymentToken
+            )).to.changeTokenBalances(nft, [user1, auction], [-1, 1]);
+
+            await expect(auction.cancelAuction(
+                token,
+                tokenId // here seller is user1 for token ID 2 but caller is owner
+            )).to.be.rejectedWith("Caller is not seller");
+        });
+
+        it("should cancel auction on valid input",async () => {
+            token = nft.address;
+            tokenId = 2;
+
+            expect((await auction.getListing(
+                token,
+                tokenId
+            )).seller).equals(user1.address);
+
+            await expect(auction.connect(user1).cancelAuction(
+                token,
+                tokenId
+            )).to.changeTokenBalances(nft, [auction, user1], [-1, 1]);
+        });
+    });
+
+    describe("Function set minimum auction duration", () => {
+        let minDuration;
+        it("should validate input parameters",async () => {
+            minDuration = MIN_AUCTION_LENGTH_IN_SECONDS - FIVE_MINS_IN_SECONDS;
+            await expect(
+                auction.setMinimumAuctionLength(minDuration)
+            ).to.be.rejectedWith("Auction length must be > 15 minutes");
+        });
+
+        it("should set new min duration",async () => {
+            minDuration = MIN_AUCTION_LENGTH_IN_SECONDS + FIVE_MINS_IN_SECONDS;
+            
+            await auction.setMinimumAuctionLength(minDuration);
+
+            expect(
+                await auction._minimumAuctionLengthInSeconds()
+            ).equal(minDuration);
+        })
     });
 });
